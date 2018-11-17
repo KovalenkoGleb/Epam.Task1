98a2596b46f07416499554f9fad71a567ed5f9ce
KovalenkoGleb <glebdalf@mail.ru>
2018-11-17 19:34:50 +0400

    Epam.Task1

---
+++ Epam.Task1/Epam.Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task1
+{
+    class Program
+    {
+        static string NtoString(int n)
+        {
+            string result = null;
+
+            for (int i = 0; i < n; i++)
+            {
+                result += i + 1;
+                if (i != n - 1)
+                    result += ", ";
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(NtoString(n));
+            Console.ReadKey();
+        }
+    }
+}
