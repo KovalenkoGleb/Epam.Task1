d20cbf955fc6e8620f5ff4a223fdab94dda76b91
KovalenkoGleb <glebdalf@mail.ru>
2018-12-02 17:13:28 +0400

    MyString

--- Epam.Task3/Epam.Task3.MyString/MyString.cs
+++ Epam.Task3/Epam.Task3.MyString/MyString.cs
@@ -10,19 +10,6 @@ namespace Epam.Task3.MyString
     {
         private char[] chars = new char[0];
 
-        public char this[int id]
-        {
-            get
-            {
-                return chars[id];
-            }
-
-            set
-            {
-                chars[id] = value;
-            }
-        }
-
         public MyString()
         {
             this.chars = new char[16];
@@ -45,7 +32,7 @@ namespace Epam.Task3.MyString
 
         public MyString(string str)
         {
-            chars = new char[str.Length];
+            this.chars = new char[str.Length];
 
             for (int i = 0; i < str.Length; i++)
             {
@@ -61,6 +48,19 @@ namespace Epam.Task3.MyString
             }
         }
 
+        public char this[int id]
+        {
+            get
+            {
+                return this.chars[id];
+            }
+
+            set
+            {
+                this.chars[id] = value;
+            }
+        }
+
         public static int Compare(MyString a, MyString b)
         {
             int minLength = Math.Min(a.Length, b.Length);
@@ -68,18 +68,26 @@ namespace Epam.Task3.MyString
             for (int i = 0; i < minLength; i++)
             {
                 if (a[i] > b[i])
+                {
                     return 1;
+                }
+
                 if (a[i] < b[i])
+                {
                     return -1;
+                }
             }
+
             if (a.Length < b.Length)
             {
                 return -1;
             }
+
             if (a.Length > b.Length)
             {
                 return 1;
             }
+
             return 0;
         }
 
@@ -89,7 +97,6 @@ namespace Epam.Task3.MyString
 
             for (int i = 0; i < a.Length; i++)
             {
-
                 temp[i] = a[i];
             }
 
@@ -103,24 +110,194 @@ namespace Epam.Task3.MyString
 
         public bool Contains(MyString b)
         {
-            for (int i = 0; i < this.Length - b.Length+1; i++) //проверить работоспособность
+            for (int i = 0; i < this.Length - b.Length + 1; i++)
             {
                 bool result = true;
                 for (int j = i; j < b.Length; j++)
                 {
-                    if (chars[j] != b[j - i])
+                    if (this.chars[j] != b[j - i])
                     {
                         result = false;
                         break;
                     }
                 }
-                if(result)
+
+                if (result)
                 {
                     return result;
                 }
             }
+
             return false;
-            
+        }
+
+        public bool StartsWith(MyString value)
+        {
+            if (value.Length > this.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < this.Length - 1; i++)
+            {
+                if (this.chars[i] != value[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool EndsWith(MyString value)
+        {
+            this.Reverse();
+            value.Reverse();
+            bool result = this.StartsWith(value);
+            this.Reverse();
+            value.Reverse();
+            return result;
+        }
+
+        public int IndexOf(char value)
+        {
+            for (int i = 0; i < this.Length; i++)
+            {
+                if (this.chars[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public int LastIndexOf(char value)
+        {
+            for (int i = this.Length - 1; i >= 0; i--)
+            {
+                if (this.chars[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public int IndexOfAny(char[] value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (this.IndexOf(value[i]) != -1)
+                {
+                    return this.IndexOf(value[i]);
+                }
+            }
+
+            return -1;
+        }
+
+        public int LastIndexOfAny(char[] value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (this.LastIndexOf(value[i]) != -1)
+                {
+                    return this.LastIndexOf(value[i]);
+                }
+            }
+
+            return -1;
+        }
+
+        public bool Equals(MyString value)
+        {
+            if (MyString.Compare(this, value) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public char[] ToChar()
+        {
+            return this.chars;
+        }
+
+        public MyString Insert(int startID, MyString value)
+        {
+            MyString result = new MyString(this.Length + value.Length);
+            for (int i = 0; i < startID; i++)
+            {
+                result[i] = this.chars[i];
+            }
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                result[startID + i] = value[i];
+            }
+
+            int j = startID;
+            for (int i = startID + value.Length; i < result.Length; i++)
+            {
+                result[i] = this.chars[j];
+                j++;
+            }
+
+            return result;
+        }
+
+        public MyString Remove(int startID, int count)
+        {
+            MyString result = new MyString(this.Length - count);
+            for (int i = 0; i < startID; i++)
+            {
+                result[i] = this.chars[i];
+            }
+
+            int j = startID;
+            for (int i = startID + count; i < this.Length; i++)
+            {
+                result[j] = this.chars[i];
+                j++;
+            }
+
+            return result;
+        }
+
+        public MyString SubMyString(int startID, int count)
+        {
+            MyString result = new MyString(count);
+            for (int i = startID; i < startID + count; i++)
+            {
+                result[i - startID] = this.chars[i];
+            }
+
+            return result;
+        }
+
+        public void ToUpper()
+        {
+            for (int i = 0; i < this.Length; i++)
+            {
+                if (this.chars[i] > 96 && this.chars[i] < 123)
+                {
+                    this.chars[i] = (char)(this.chars[i] - 32);
+                }
+            }
+        }
+
+        public void ToLower()
+        {
+            for (int i = 0; i < this.Length; i++)
+            {
+                if (this.chars[i] > 64 && this.chars[i] < 91)
+                {
+                    this.chars[i] = (char)(this.chars[i] + 32);
+                }
+            }
         }
 
         public override string ToString()
@@ -128,9 +305,21 @@ namespace Epam.Task3.MyString
             var strbuild = new StringBuilder();
             for (int i = 0; i < this.Length; i++)
             {
-                strbuild.Append(chars[i]);
+                strbuild.Append(this.chars[i]);
             }
+
             return strbuild.ToString();
+        }
+
+        private void Reverse()
+        {
+            char[] temp = new char[this.Length];
+            for (int i = 0; i < this.Length; i++)
+            {
+                temp[i] = this.chars[this.Length - i - 1];
+            }
+
+            this.chars = temp;
         }
     }
 }
--- Epam.Task3/Epam.Task3.MyString/Program.cs
+++ Epam.Task3/Epam.Task3.MyString/Program.cs
@@ -10,24 +10,196 @@ namespace Epam.Task3.MyString
     {
         public static void Main(string[] args)
         {
-            char[] ch = new char[10];
-            for (int i = 0; i < ch.Length; i++)
+            string stringstr1;
+            Console.Write($"Welcom to MyString Test Application{Environment.NewLine}Enter string line: ");
+            stringstr1 = Console.ReadLine();
+            MyString mystr1 = new MyString(stringstr1);
+            Console.WriteLine($"MyString from string line is: {mystr1}{Environment.NewLine}MyString to char is:{mystr1.ToChar()}{Environment.NewLine}It's length is {mystr1.Length}{Environment.NewLine}");
+
+            Console.Write("Enter another two MyString lines to compare them. First one: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Second one: ");
+            MyString mystr2 = new MyString(Console.ReadLine());
+            switch (MyString.Compare(mystr1, mystr2))
             {
-                ch[i] = (char)(i + 64);                
-                //Console.Write(ch[i]);
+                case 1:
+                    Console.WriteLine("First one is bigger");
+                    break;
+                case -1:
+                    Console.WriteLine("Second one is bigger");
+                    break;
+                case 0:
+                    Console.WriteLine("They are the same");
+                    break;
             }
 
             Console.WriteLine();
 
-            MyString mystring1 = new MyString("abc");
-            MyString mystring2 = new MyString("bc");
+            Console.Write("Enter another two MyString lines to concatenate them. First one: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Second one: ");
+            mystr2 = new MyString(Console.ReadLine());
+            Console.WriteLine($"The concatenation is: {MyString.Concat(mystr1, mystr2)}");
+            Console.WriteLine();
 
+            Console.Write("Enter another two MyString lines to find out, if they are equal. First one: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Second one: ");
+            mystr2 = new MyString(Console.ReadLine());
+            if (mystr1.Equals(mystr2))
+            {
+                Console.WriteLine("They are equal");
+            }
+            else
+            {
+                Console.WriteLine("They are not equal");
+            }
 
-           
-            Console.WriteLine("abc".Contains("bc"));
-            //Console.WriteLine(mystring1);
-            Console.WriteLine(mystring1.Contains(mystring2));
-            
+            Console.WriteLine();
+
+            Console.Write("Enter another two MyString lines to find if the first contains second. First one: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Second one: ");
+            mystr2 = new MyString(Console.ReadLine());
+            if (mystr1.Contains(mystr2))
+            {
+                Console.WriteLine("It does");
+            }
+            else
+            {
+                Console.WriteLine("It doesn't");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter another two MyString lines to find if the first starts with second. First one: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Second one: ");
+            mystr2 = new MyString(Console.ReadLine());
+            if (mystr1.StartsWith(mystr2))
+            {
+                Console.WriteLine("It does");
+            }
+            else
+            {
+                Console.WriteLine("It doesn't");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter another two MyString lines to find if the first ends with second. First one: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Second one: ");
+            mystr2 = new MyString(Console.ReadLine());
+            if (mystr1.EndsWith(mystr2))
+            {
+                Console.WriteLine("It does");
+            }
+            else
+            {
+                Console.WriteLine("It doesn't");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter MyString to find the index of a sybmbol. MyString: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Sybmbol: ");
+            char symbol = char.Parse(Console.ReadLine());
+            if (mystr1.IndexOf(symbol) == -1)
+            {
+                Console.WriteLine("There is no such symbol");
+            }
+            else
+            {
+                Console.WriteLine(mystr1.IndexOf(symbol));
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter MyString to find the last index of a sybmbol. MyString: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Sybmbol: ");
+            symbol = char.Parse(Console.ReadLine());
+            if (mystr1.LastIndexOf(symbol) == -1)
+            {
+                Console.WriteLine("There is no such symbol");
+            }
+            else
+            {
+                Console.WriteLine(mystr1.LastIndexOf(symbol));
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter MyString to find the first index of any symbol in char array. MyString: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Sybmbols: ");
+            char[] symbols = Console.ReadLine().ToCharArray();
+            if (mystr1.IndexOfAny(symbols) == -1)
+            {
+                Console.WriteLine("There is no such symbols");
+            }
+            else
+            {
+                Console.WriteLine(mystr1.IndexOfAny(symbols));
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter MyString to find the last index of any symbol in char array. MyString: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Sybmbols: ");
+            symbols = Console.ReadLine().ToCharArray();
+            if (mystr1.LastIndexOfAny(symbols) == -1)
+            {
+                Console.WriteLine("There is no such symbols");
+            }
+            else
+            {
+                Console.WriteLine(mystr1.LastIndexOfAny(symbols));
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Enter MyString to make it to upper: ");
+            mystr1 = new MyString(Console.ReadLine());
+            mystr1.ToUpper();
+            Console.WriteLine($"The result is: {mystr1}");
+            Console.WriteLine();
+
+            Console.Write("Enter MyString to make it to lower: ");
+            mystr1 = new MyString(Console.ReadLine());
+            mystr1.ToLower();
+            Console.WriteLine($"The result is: {mystr1}");
+            Console.WriteLine();
+
+            Console.Write("Enter another two MyString lines and position to insert second in the first. First one: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Second one: ");
+            mystr2 = new MyString(Console.ReadLine());
+            Console.Write("Position: ");
+            int position = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The result is: {mystr1.Insert(position, mystr2)}");
+            Console.WriteLine();
+
+            Console.Write("Enter another MyString line and start and end positions to remove elements. MyString: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Start: ");
+            position = int.Parse(Console.ReadLine());
+            Console.Write("End: ");
+            int position1 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The result is: {mystr1.Remove(position, position1)}");
+            Console.WriteLine();
+
+            Console.Write("Enter another MyString line and start and end positions to generate SubMyString. MyString: ");
+            mystr1 = new MyString(Console.ReadLine());
+            Console.Write("Start: ");
+            position = int.Parse(Console.ReadLine());
+            Console.Write("End: ");
+            position1 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The result is: {mystr1.SubMyString(position, position1)}");
+            Console.WriteLine();
         }
     }
 }
