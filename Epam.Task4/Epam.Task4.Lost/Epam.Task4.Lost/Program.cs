bf2ef74efb749cfe50a08ff376b458fd22f174b8
KovalenkoGleb <glebdalf@mail.ru>
2018-12-06 15:17:59 +0400

    Task4

---
+++ Epam.Task4/Epam.Task4.Lost/Epam.Task4.Lost/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task4.Lost
+{
+    public class Program
+    {
+        public static int SearchOfLast(Queue<int> roundOfPeople)
+        {
+            bool flag = true;
+            while (roundOfPeople.Count != 1)
+            {
+                if (flag)
+                {
+                    roundOfPeople.Enqueue(roundOfPeople.Dequeue());
+                    flag = !flag;
+                }
+                else
+                {
+                    roundOfPeople.Dequeue();
+                    flag = !flag;
+                }
+            }
+
+            return roundOfPeople.Peek();
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Queue<int> roundOfPeople = new Queue<int>();
+                Console.WriteLine("Enter n: ");
+                int.TryParse(Console.ReadLine(), out int n);
+                for (int i = 0; i < n; i++)
+                {
+                    roundOfPeople.Enqueue(i);
+                }
+
+                Console.WriteLine($"Last is {SearchOfLast(roundOfPeople)}");
+            }
+            catch
+            {
+                Console.WriteLine("Error. Wrong input");
+            }
+        }
+    }
+}
---
+++ Epam.Task4/Epam.Task4.Lost/Epam.Task4.WordFrequency/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Task4.WordFrequency
+{
+    public class Program
+    {
+        public static void WordFounder(string s, ref Dictionary<string, int> wordFreq)
+        {
+            s.ToLower();
+            char[] separators = new char[2] { ' ', '.' };
+            while (s.Length != 0)
+            {
+                int i_sep = s.IndexOfAny(separators);
+                if (i_sep == -1)
+                {
+                    i_sep = s.Length;
+                }
+
+                if (!wordFreq.ContainsKey(s.Substring(0, i_sep)))
+                {
+                    if (s.Substring(0, i_sep) != " ")
+                    {
+                        wordFreq.Add(s.Substring(0, i_sep), 1);
+                    }
+                }
+                else
+                {
+                    wordFreq[s.Substring(0, i_sep)]++;
+                }
+
+                if (i_sep >= s.Length)
+                {
+                    i_sep = s.Length - 1;
+                }
+
+                s = s.Remove(0, i_sep + 1);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Dictionary<string, int> wordFreq = new Dictionary<string, int>();
+
+            StreamReader sr = new StreamReader("input.txt");
+            string s = sr.ReadToEnd();
+            WordFounder(s, ref wordFreq);
+
+
+            foreach (var item in wordFreq)
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+            }
+        }
+    }
+}
