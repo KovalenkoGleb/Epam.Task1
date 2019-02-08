1ee23a524096e9c883fef5c8c93799c15d6b55f5
KovalenkoGleb <glebdalf@mail.ru>
2019-02-08 13:00:00 +0400

    Task11 not ended

---
+++ Epam.Task7/Epam.Task11.WebUI/js/Scripts.js
@@ -0,0 +1,19 @@
+﻿var addUserBtn = document.getElementById('addUser'),
+    SbmtUser = document.getElementById('SubmitUser');
+//    urls = ['~/pages/AddUserPage.cshtml']
+
+function redirect(url) {
+    location = url;
+}
+
+addUserBtn.onclick = function () {
+    redirect('/pages/AddUserPage.cshtml');
+}
+
+SbmtUser.onclick = function () {
+    
+}
+
+
+
+
