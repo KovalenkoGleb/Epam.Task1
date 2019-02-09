15d2bbd9be70ccead0c3fd16fad2d5782c3a91a8
KovalenkoGleb <glebdalf@mail.ru>
2019-02-09 14:59:15 +0400

    Task11 update

--- Epam.Task7/Epam.Task11.WebUI/js/Scripts.js
+++ Epam.Task7/Epam.Task11.WebUI/js/Scripts.js
@@ -1,6 +1,13 @@
 ﻿var addUserBtn = document.getElementById('addUser'),
-    SbmtUser = document.getElementById('SubmitUser');
-//    urls = ['~/pages/AddUserPage.cshtml']
+    showUsersBtn = document.getElementById('showUsers'),
+    delUserBtn = document.getElementById('delUser'),
+    getUserbyIdBtn = document.getElementById('getUserById'),
+    addAwardBtn = document.getElementById('addAward'),
+    delAwardBtn = document.getElementById('delAward'),
+    showAwardsBtn = document.getElementById('showAwards'),
+    getAwardByIdBtn = document.getElementById('getAwardById'),
+    addAwardToUserBtn = document.getElementById('addAwardToUser');
+
 
 function redirect(url) {
     location = url;
@@ -10,10 +17,34 @@ addUserBtn.onclick = function () {
     redirect('/pages/AddUserPage.cshtml');
 }
 
-SbmtUser.onclick = function () {
-    
+showUsersBtn.onclick = function () {
+    redirect('/pages/ShowAllUsers.cshtml');
 }
 
+delUserBtn.onclick = function () {
+    redirect('/pages/DeleteUser.cshtml');
+}
 
+getUserbyIdBtn.onclick = function () {
+    redirect('/pages/GetUserById.cshtml');
+}
 
+addAwardBtn.onclick = function () {
+    redirect('/pages/AddAward.cshtml');
+}
 
+delAwardBtn.onclick = function () {
+    redirect('/pages/DeleteAward.cshtml');
+}
+
+showAwardsBtn.onclick = function () {
+    redirect('/pages/ShowAwards.cshtml');
+}
+
+getAwardByIdBtn.onclick = function () {
+    redirect('/pages/GetAwardById.cshtml');
+}
+
+addAwardToUserBtn.onclick = function () {
+    redirect('/pages/AddAwardToUser.cshtml');
+}
