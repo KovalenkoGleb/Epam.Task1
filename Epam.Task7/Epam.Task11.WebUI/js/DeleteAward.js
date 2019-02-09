c7ba8d62da2538abe9f813164cc22773c78b529b
KovalenkoGleb <glebdalf@mail.ru>
2019-02-09 16:07:12 +0400

    task 11 11.1-11.2

---
+++ Epam.Task7/Epam.Task11.WebUI/js/DeleteAward.js
@@ -0,0 +1,18 @@
+﻿var sbmtBtn = document.getElementById('Sbmt'),
+    realSbmtBtn = document.getElementById('realSbmt');
+realSbmtBtn.hidden = 1;
+
+function redirect(url) {
+    location = url;
+}
+
+sbmtBtn.onclick = function () {
+    res = confirm("Do you really want to delete this award? Press submit again to do it.");
+    if (res)
+        realSbmtBtn.click();
+    else
+        redirect('/pages/DeleteAward.cshtml');
+
+    sbmtBtn.hidden = 1;
+    realSbmtBtn.hidden = 0;
+}
